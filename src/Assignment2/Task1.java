0b6126758bd8ffd67cdcb600854ba9a0f68bd6c7
Dericeda <nurasyl.asanov@mail.ru>
2024-12-16 18:20:31 +0500

    Adding 1 task;deleting main.java

---
+++ src/Assignment2/Task1.java
@@ -0,0 +1,12 @@
+package Assignment2;
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your temperature ");
+        int temperature = scanner.nextInt();
+        if(temperature>=0){
+            System.out.print("it’s warm outside");
+        }else
+            System.out.print("it’s cold outside");
+    }}
